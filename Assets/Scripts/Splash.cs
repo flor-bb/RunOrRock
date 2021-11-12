5f89d4535cba77b0febe9e1a558777d561035f7d
Florian Bruderer <florian.bruderer@gmail.com>
2021-11-12 19:12:55 +0100

    Added a splashscreen

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Splash : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine(FinishSplashScreen());
+    }
+
+
+
+    private IEnumerator FinishSplashScreen()
+    {
+
+        yield return new WaitForSeconds(4f);
+        SceneManager.LoadScene("Main_Menu");
+
+    }
+
+
+
+}
